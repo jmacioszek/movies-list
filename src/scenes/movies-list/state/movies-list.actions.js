5bb0bbace1820f67120ad846614631c158621d64
Jakub Macioszek <jakubmacioszek@Jakubs-MBP.home>
2018-08-26 23:58:56 +0100

    feature/movies-list-screen

---
+++ src/scenes/movies-list/state/movies-list.actions.js
@@ -0,0 +1,37 @@
+import { createActionTypes } from '../../../state/utils';
+
+const name = 'moviesList';
+
+const types = createActionTypes(
+  {
+    async: ['FETCH_MOVIES'],
+  },
+  name,
+);
+
+const fetchMovies = () => ({
+  type: types.FETCH_MOVIES,
+});
+
+const fetchMoviesStarted = () => ({
+  type: types.FETCH_MOVIES_STARTED,
+});
+
+const fetchMoviesSuccess = ({ movies }) => ({
+  type: types.FETCH_MOVIES_SUCCESS,
+  payload: {
+    movies,
+  },
+});
+
+const actions = {
+  fetchMovies,
+  fetchMoviesSuccess,
+  fetchMoviesStarted,
+};
+
+export default {
+  types,
+  actions,
+  name,
+};
